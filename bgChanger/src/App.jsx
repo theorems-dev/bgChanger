87069b9a4cd45aa0a24631a7290857d2e28753c2
Kritiraj <xenny.irl@gmail.com>
2025-01-03 11:07:23 +0530

    Created and Styles all elements.

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [color, setColor] = useState("black");
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className="w-full h-screen flex justify-center items-center font-mono text-3xl duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <h1 className="text-white font-bold">Background</h1>
+        <div className="fixed bg-white min-h-14 w-3/5 max-w-3xl rounded-3xl bottom-12 text-black text-lg flex justify-around items-center">
+          <button
+            className="outline-none min-h-10 min-w-24 bg-green-600 rounded-3xl align-center"
+            onClick={() => setColor("green")}
+          >
+            Green
+          </button>
+          <button
+            className="outline-none min-h-10 min-w-24 bg-blue-600 rounded-3xl text-center"
+            onClick={() => setColor("blue")}
+          >
+            Blue
+          </button>
+          <button
+            className="outline-none min-h-10 min-w-24 bg-red-600 rounded-3xl text-center"
+            onClick={() => setColor("red")}
+          >
+            Red
+          </button>
+          <button
+            className="outline-none min-h-10 min-w-24 bg-yellow-400 rounded-3xl text-center"
+            onClick={() => setColor("yellow")}
+          >
+            Yellow
+          </button>
+          <button
+            className="outline-none min-h-10 min-w-24 bg-cyan-400 rounded-3xl text-center"
+            onClick={() => setColor("cyan")}
+          >
+            Cyan
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
